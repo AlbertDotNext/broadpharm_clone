ae1ae9989b033170851bd42bf5595280166e7329
Albert <albertwu0001@gmail.com>
2020-12-01 22:34:51 -0800

    add sticky header

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -5,34 +5,65 @@ import EmailIcon from '@material-ui/icons/Email';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import HamburgerMenu from './HamburgerMenu';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
 function ContactInfo(){
   return (
-    <div className="Contact">
+    <StickyHeader
+    header={
+      <div className="Contact">
 
-     <div className="mobile-view-hamburger">
-      <HamburgerMenu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
-     </div>
+        <div className="mobile-view-hamburger">
+          <HamburgerMenu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
+        </div>
 
-      <ul className="nav-contact">
-        <li>
+        <ul className="nav-contact">
+          <li>
 
-          <a className="nav-link" href="tel: 858-677-6760"><Phone/>858-677-6760</a>
-        </li>
-        <li>
-          <a className="nav-link" href="mailto:sales@broadpharm.com"><EmailIcon/>sales@broadpharm.com</a>
-        </li>
-        <li>
-          <a className="nav-link" href="https://www.broadpharm.com/web/signin.php"><LockOpenIcon/>Login</a>
-        </li>
-        <li className="nav-link">
-          <a className="" href="https://www.broadpharm.com/carts"><ShoppingCartIcon/></a>
-        </li>
-      </ul>
+            <a className="nav-link" href="tel: 858-677-6760"><Phone/>858-677-6760</a>
+          </li>
+          <li>
+            <a className="nav-link" href="mailto:sales@broadpharm.com"><EmailIcon/>sales@broadpharm.com</a>
+          </li>
+          <li>
+            <a className="nav-link" href="https://www.broadpharm.com/web/signin.php"><LockOpenIcon/>Login</a>
+          </li>
+          <li className="nav-link">
+            <a className="" href="https://www.broadpharm.com/carts"><ShoppingCartIcon/></a>
+          </li>
+        </ul>
 
-    </div>
+      </div>
+    }
+    >
+
+      <div className="Contact">
+
+      <div className="mobile-view-hamburger">
+        <HamburgerMenu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
+      </div>
+
+        <ul className="nav-contact">
+          <li>
+
+            <a className="nav-link" href="tel: 858-677-6760"><Phone/>858-677-6760</a>
+          </li>
+          <li>
+            <a className="nav-link" href="mailto:sales@broadpharm.com"><EmailIcon/>sales@broadpharm.com</a>
+          </li>
+          <li>
+            <a className="nav-link" href="https://www.broadpharm.com/web/signin.php"><LockOpenIcon/>Login</a>
+          </li>
+          <li className="nav-link">
+            <a className="" href="https://www.broadpharm.com/carts"><ShoppingCartIcon/></a>
+          </li>
+        </ul>
+
+      </div>
+    </StickyHeader>
   )
 
 }
 
-export default ContactInfo
+export default ContactInfo;
--- src/components/MegaMenu.js
+++ src/components/MegaMenu.js
@@ -3,11 +3,16 @@ import logo from '../logo.png';
 import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 import './MegaMenu.css'
 import SearchIcon from '@material-ui/icons/Search';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
 
 function MegaMenu() {
   return (
-    <div className="mega">
+    <StickyHeader
+    backgroundColor={"white"}
+    header={
+      <div className="mega">
 
 
         <Navbar expand="lg" className="megabar">
@@ -45,6 +50,48 @@ function MegaMenu() {
         </Navbar>
 
     </div>
+    }
+    >
+      <div className="mega">
+
+
+        <Navbar expand="lg" className="megabar">
+          <a className="logo" href="https://www.broadpharm.com">
+            <img src={logo} alt="BroadPharm"/>
+          </a>
+
+          <Navbar.Collapse className="basic-navbar-nav">
+            <Nav className="mega-navbar">
+              <Nav.Link href="#home">HOME</Nav.Link>
+
+              <NavDropdown title="PRODUCTS" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">BY CATEGORY</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">BY APPLICATION</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link">ORDER</Nav.Link>
+              <NavDropdown title="TECHNICAL" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">PROTOCOLS</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">QA/QC</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="ABOUT" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">COMPANY INFO</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">NEWS ROOM</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="CONTACT" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">UNITED STATES</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">DISTRIBUTORS</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline className="mega-search-box">
+              <FormControl type="text" placeholder="Search" className="search-box" />
+              <Button className="search-box-button" ><SearchIcon/></Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+
+        </div>
+    </StickyHeader>
+
   )
 }
 
